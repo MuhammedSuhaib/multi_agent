dcaf9955a988d073c47528ef12249b9319db00bb
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-09-04 18:14:19 +0500

    README with mutable vs immutable examples

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# Logs
-![image](image.png)
+# Mutable pass-by-reference/id
+
+```py
+a = [1, 2]
+b = a
+print(f"a : {id(a)}") # a : 2103974454592
+print(f"b : {id(b)}") # b : 2103974454592
+b[0] = 3 # we replace the first element of only ** b ** with 3 , but a also reflects the changes
+print(f"a : {a}") # a : [3, 2]
+print(f"b : {b}") # b : [3, 2]
+
+```
+
+→ Both `a` and `b` point to the same list (same memory `id`).  
+Changing `b` also changes `a`.
+
+### Output:
+
+```py
+a : 2103974454592 # same id
+b : 2103974454592 # same id
+a : [3, 2]
+b : [3, 2]
+```
+# On Other Hand
+
+If you do
+
+```py
+b = a.copy()
+```
+
+→ `b` becomes a new list with a different `id`.         
+Changing `b` won’t affect `a`.
+
+```py
+
+a : [1, 2]
+b : [3, 2]
+```
+
+# Immutable pass-by-value
+
+```py
+a = 10
+b = a
+b = 20
+
+print(f"a : {id(a)}")
+print(f"b : {id(b)}")
+```
+### output:
+```py
+a : 140715980039368
+b : 140715980039688
+```
+
+* **Immutable (int, str, tuple, etc.)** → assigning a new value makes a new object, original stays same.
+* **Mutable (list, dict, set, etc.)** → changes affect all references to that object.
--- main.py
+++ main.py
@@ -1,47 +1,39 @@
-from agents import RunConfig, Runner, set_tracing_export_api_key, trace
-from schemas.schemas import HotelContext
-from simple_agents.aagents import Triage_Agent
-
-# from openai.types.responses import ResponseTextDeltaEvent
-from agents.exceptions import (
-    InputGuardrailTripwireTriggered,
-    OutputGuardrailTripwireTriggered,
-)
+from agents import Runner, Agent, ModelSettings
 import asyncio
 import os
 from dotenv import load_dotenv
+from configs.config import model_config
 
 load_dotenv()
 Tracing_key = os.getenv("Tracing_key")
 
 
+# Triage Agent — decides who should handle the query
+Triage_Agent = Agent(
+    name="Triage_Agent", instructions="reply in 1 word", model=model_config
+)
+
+new_agent = Triage_Agent.clone(name="NewAgent")
+print(id(Triage_Agent))
+print(id(new_agent))
+#  ----------------------- Agents
+
+print("old_agent : ", Triage_Agent)
+print("\n\n====================================\n\n")
+print("new Agent : ", new_agent)
+
+#  ----------------------- Prints
+print(Triage_Agent.model_settings)
+print("====================================")
+
+
+#  ----------------------- Runner
 async def main():
-    set_tracing_export_api_key(Tracing_key)
-    with trace(workflow_name="Assi9", disabled=False):
-        try:
-            while True:
-                try:
-                    user_query = input("\nUser: ")
-                except EOFError:
-                    break
-
-                # Now triage_agent already knows its handoff targets
-                output = await Runner.run(
-                    starting_agent=Triage_Agent,
-                    input=user_query,
-                    run_config=RunConfig(
-                        workflow_name="xplr", trace_id="7", group_id="0"
-                    ),
-                )
-                print(output.final_output)
-                # async for event in output.stream_events():
-                #     if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
-                #         print(event.data.delta, end="", flush=True)
-        except InputGuardrailTripwireTriggered as e:
-            print("Guardrail blocked this input:🔺", e)
-
-        except OutputGuardrailTripwireTriggered as e:
-            print("Guardrail blocked this output:🔺", e)
+    output = await Runner.run(
+        starting_agent=Triage_Agent,
+        input="hi",
+    )
+    print(output.final_output)
 
 
 asyncio.run(main())
